7c3f94832a8d5add76df20d39411f5f0da8421f0
MxAli-56 <muhammadaliasghar905@gmail.com>
2025-08-21 13:03:15 +0500

    Add JWT authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+
+function authMiddleware(req, res, next){
+    const authHeader = req.headers["authorization"]
+
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({message: "No token... Access denied!"})
+    }
+
+try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    req.user = decoded
+
+    next()
+} catch (error) {
+    return res.status(401).json({message: "Invalid or expired token"})
+}
+}
+
+module.exports = authMiddleware;
